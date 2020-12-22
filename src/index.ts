6fb03b2f693428b11c1dd79bae8498b398f52790
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-12-22 18:14:38 +0800

    ..

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import { getUserToken, getUserInfo, refreshUserToken } from './userAuth';
 import { handleOrder, getOrderStatus } from './order';
 import {
-  IAccessData,
   IUserData,
   IUserAccessTokenData,
   IOrderData,
@@ -13,22 +12,18 @@ import {
   IGetHostedResponse,
   IGetBalanceResponse,
   ISaveDataResponse,
-  IAppAccessTokenData,
 } from './types';
 import { autopayment } from './autopayment';
 import { saveData, getHostedAccount, hostedAccountBalance } from './saveData';
 import { getAppAccessToken } from './appAuth';
 class DotWallet {
-  private CLIENT_ID: string;
-  private SECRET: string;
+  private CLIENT_ID: string = '';
+  private SECRET: string = '';
   private appAccessToken: string | undefined;
-  start = async () => {
-    // functions that us the app token need to be repopulated with the token each time. ones that only use the secrets are static and don't
-    getAppAccessToken(this.CLIENT_ID, this.SECRET, this, true);
 
-    // create a function that updates the functions that use the appAccessToken
-    setInterval(() => {});
-    // this.refreshAccess = refreshAccess(this.CLIENT_ID, this);
+  refreshAppAuth = async () => {};
+  populateStaticMethods = () => {
+    this.getUserToken = getUserToken(this.CLIENT_ID, this.SECRET);
     this.handleOrder = handleOrder(this.CLIENT_ID, this.SECRET);
     this.getOrderStatus = getOrderStatus(this.CLIENT_ID, this.SECRET);
     this.autopayment = autopayment(this.SECRET);
@@ -36,35 +31,61 @@ class DotWallet {
     this.hostedAccountBalance = hostedAccountBalance(this.CLIENT_ID, this.SECRET);
     this.saveData = saveData(this.CLIENT_ID, this.SECRET);
   };
+  populateTokenMethods = () => {
+    // when they return an expired token error, recursive call up to 2 times.
+  };
+  resetAppToken = async (CLIENT_ID: string, SECRET: string, that: this) => {
+    await getAppAccessToken(this.CLIENT_ID, this.SECRET, that);
+    this.populateTokenMethods();
+  };
+  init = async (CLIENT_ID: string, SECRET: string) => {
+    this.CLIENT_ID = CLIENT_ID;
+    this.SECRET = SECRET;
+    await getAppAccessToken(this.CLIENT_ID, this.SECRET, this);
+    // functions that us the app token need to be repopulated with the token each time. ones that only use the secrets are static and don't
+    this.populateStaticMethods();
+    this.populateTokenMethods();
+    // the app access token will expire every 2 hours.
+    setInterval(async () => {
+      this.resetAppToken(this.CLIENT_ID, this.SECRET, this);
+    }, 7200000);
+    // this.refreshAccess = refreshAccess(this.CLIENT_ID, this);
+  };
 
-  /** @param redirectWithQueries If you'd like to have express send the response to redirect to a url, carrying the user data queries with it, than include a redirect URL string here
+  /**
+   * @param code the code from the login challenge at https://api.ddpurse.com/v1/oauth2/authorize?client_id=...
+   * @param redirectUri Must match provided redirect uri in the login challenge
    * @param log whether to log events
-   * @example app.get('/auth', Dotwallet.handleAuthResponse('/frontend-landing-page', true)); app.get('/auth', dotwallet.handleAuthResponse().then(result=>{const userData = result.userData; const accessToken = result.accessData.access_token;}));
-   * @returns {object} { userData: {user_open_id, ...}, accessData:{ refresh_token, expires_in, access_token }}
+   * @example 
+  app.post('/auth', async (req, res, next) => {
+    const authTokenData = await dotwallet.getUserToken(req.body.code, req.body.redirect_uri, true);
+    res.json({ ...authTokenData });
+  });
+   * @returns {object} { access_token: 'JWT access token', expires_in: number, token_type: 'Bearer', refresh_token: string, scope: string
+}
    */
   getUserToken: (
     code: string,
-    redirectUri?: string | undefined,
+    redirectUri: string,
     log?: false | undefined,
-  ) => Promise<IUserAccessTokenData | undefined>;
+  ) => Promise<IUserAccessTokenData | undefined> = getUserToken(this.CLIENT_ID, this.SECRET);
 
-  getUserInfo: (userAccessToken: string, log?: false | undefined) => Promise<IUserData | undefined>;
-
-  /**
-   * @summary Use your refresh token to get back an access token and a new refresh token
-   * @param { string } refreshToken
-   * @returns { object|Error } { refresh_token, expires_in, access_token }
-   */
-  // refreshAccess: (refreshToken: string) => Promise<IAccessData | Error | undefined>;
+  getUserInfo: (userAccessToken: string, log?: false | undefined) => Promise<IUserData | undefined> = getUserInfo;
 
   /**
    * @summary sign an order with your developer key, and send it to dotwallet servers to get an order_sn
    * @param { IOrderData } orderData a valid order as a js object (see the docs or this IorderData type)
    * @returns { string } the order_sn
    */
-  handleOrder: (orderData: IOrderData, log?: boolean | undefined) => Promise<string | Error | undefined>;
+  handleOrder: (orderData: IOrderData, log?: boolean | undefined) => Promise<string | Error | undefined> = handleOrder(
+    this.CLIENT_ID,
+    this.SECRET,
+  );
 
-  getOrderStatus: (merchant_order_sn: string) => Promise<IOrderStatusInfo | Error | undefined>;
+  getOrderStatus: (merchant_order_sn: string) => Promise<IOrderStatusInfo | Error | undefined> = getOrderStatus(
+    this.CLIENT_ID,
+    this.SECRET,
+  );
 
   /**
    * @summary Send out a payment on behalf of a wallet that has authorized auto-payments
@@ -74,14 +95,17 @@ class DotWallet {
   autopayment: (
     orderData: IAutoPaymentOrder,
     log?: boolean | undefined,
-  ) => Promise<IOrderResponseData | Error | undefined>;
+  ) => Promise<IOrderResponseData | Error | undefined> = autopayment(this.SECRET);
 
-  getHostedAccount: (coinType?: string, log?: boolean | undefined) => Promise<IGetHostedResponse | Error | undefined>;
+  getHostedAccount: (
+    coinType?: string,
+    log?: boolean | undefined,
+  ) => Promise<IGetHostedResponse | Error | undefined> = getHostedAccount(this.CLIENT_ID, this.SECRET);
 
   hostedAccountBalance: (
     coinType?: string,
     log?: boolean | undefined,
-  ) => Promise<IGetBalanceResponse | Error | undefined>;
+  ) => Promise<IGetBalanceResponse | Error | undefined> = hostedAccountBalance(this.CLIENT_ID, this.SECRET);
 
   /**
    * @param {dataType} dataType 0 for string and 1 for rawhex. If you select 0 (the default) we will JSON.stringify() the data to be saved on chain
@@ -91,25 +115,7 @@ class DotWallet {
     data: any,
     dataType?: dataType,
     log?: boolean | undefined,
-  ) => Promise<ISaveDataResponse | Error | undefined>;
-
-  constructor(appId: string, secret: string) {
-    this.CLIENT_ID = appId;
-    this.SECRET = secret;
-    this.appAccessToken = undefined;
-    this.start();
-    this.getUserToken = getUserToken(this.CLIENT_ID, this.SECRET);
-    this.getUserInfo = getUserInfo;
-    // this.refreshAccess = refreshAccess(this.CLIENT_ID);
-    this.handleOrder = handleOrder(this.CLIENT_ID, this.SECRET);
-    this.getOrderStatus = getOrderStatus(this.CLIENT_ID, this.SECRET);
-    this.autopayment = autopayment(this.SECRET);
-    this.getHostedAccount = getHostedAccount(this.CLIENT_ID, this.SECRET);
-    this.hostedAccountBalance = hostedAccountBalance(this.CLIENT_ID, this.SECRET);
-    this.saveData = saveData(this.CLIENT_ID, this.SECRET);
-  }
+  ) => Promise<ISaveDataResponse | Error | undefined> = saveData(this.CLIENT_ID, this.SECRET);
 }
 
-const caller = (appId: string, secret: string) => new DotWallet(appId, secret);
-
-export = caller;
+export = DotWallet;
