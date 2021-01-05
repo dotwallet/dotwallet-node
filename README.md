20366215ba25d23c559a028b46c68fea43447ec0
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2021-01-05 12:44:21 +0800

    readme and readability

--- README.md
+++ README.md
@@ -1,33 +1,73 @@
 # dotwallet-node
 
-SDK for backend servers interacting with DotWallet for Developers APIs.
+An SDK for quickly building a server that uses DotWallet for Developers's APIs for logins, payments, and other blockchain services.
 
-\*\*\* Currently in production. Do not use in a project. finished app and user auth.
-to do:
+This SDK automatically handles the refreshing of user and app access tokens and provides common error handling.
 
-- refresh user token?
-- order pay
-- autopay
-- save data
-- examine transactions, other APIS
-- clean up types
-- better sdk docs/tutorial
+## To use
 
-## dev build
+```bash
+npm install dotwallet-node
+```
 
-`npm run build`
-will build to 'lib' folder
+```js
+const express = require('express'); // or Koa, etc.
+const app = express();
 
-## Example
+const DotWallet = require('dotwallet-node');
+const dotwallet = new DotWallet();
+// Initialize DotWallet with your client_id and secret
+dotwallet.init('<YOUR_CLIENT_ID>', '<YOUR_CLIENT_SECRET>', true);
+// Note: The last param of each call 'true' is for logging. It can be omitted if you don't want to see logs.
 
-to see how to use the SDK, check out:
-examples/server/src/index.js
+// ***** Handle user login and getting user info. *****
+app.post('/auth', async (req, res) => {
+  const authTokenData = await dotwallet.getUserToken(req.body.code, req.body.redirect_uri);
+  const userAccessToken = authTokenData.access_token;
+  const userInfo = await dotwallet.getUserInfo(userAccessToken);
+  res.json({ ...userInfo, ...authTokenData });
+});
 
-### run example
+// ***** Handle payment order *****
+app.post('/create-order', async (req, res) => {
+  const order = { ...req.body }; // must be valid order format, examine the IPaymentOrder type, or the dotwallet docs page for format schema
+  const orderID = await dotwallet.getOrderID(req.body);
+  res.json({ order_id: orderID });
+});
+
+// ***** Handle Automatic payment order *****
+app.post('/autopay', async (req, res) => {
+  // Note: if the user balance is too low, this will send an error back. Redirect the user to move funds to their autopay account, then retry the transaction.
+  const orderResultData = await dotwallet.autoPay(req.body);
+  res.json(orderResultData);
+});
+
+// ***** Save data to the BSV blockchain *****
+app.post('/save-data', async (req, res) => {
+  const saveDataResult = await dotwallet.saveData(req.body.dataToSave, req.body.userID);
+  // You will receive back a txid. In a real app this txid would be stored to DB to later check the saved data on the blockchain
+});
+```
+
+## Examples
+
+For more complete examples, see the example server in the package in the [`examples/server/src/index.js`](https://github.com/dotwallet/dotwallet-node/blob/main/examples/server/src/index.js) file.
+
+### build
 
 ```bash
 npm i
 npm run build
+```
+
+will build to 'lib' folder
+
+### run example
+
+build sdk first, then example
+
+```bash
 cd examples/server/
+npm i
 npm run dev
 ```
--- examples/server/src/index.js
+++ examples/server/src/index.js
@@ -13,6 +13,7 @@ const appUrl = 'http://' + ip.address() + ':' + PORT;
 const YOUR_CLIENT_SECRET = process.env.CLIENT_SECRET;
 const YOUR_CLIENT_ID = process.env.CLIENT_ID;
 const DotWallet = require('../../../lib/index.js');
+// const DotWallet = require('dotwallet-node'); //in a real app
 const dotwallet = new DotWallet();
 dotwallet.init(YOUR_CLIENT_ID, YOUR_CLIENT_SECRET, true);
 app.use(cors());
@@ -23,7 +24,7 @@ app.use(express.static('src'));
 app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 });
-// client-side page to receive the code after user confirms login
+// client-side page to receive the code after user confirms login. Could be done on the same login page, but separated here to show steps
 app.get('/log-in-redirect', async (req, res) => {
   res.sendFile(path.join(__dirname + '/views/log-in-redirect.html'));
 });
@@ -33,18 +34,18 @@ app.get('/logged-in', async (req, res) => {
 app.get('/store-front', async (req, res) => {
   res.sendFile(path.join(__dirname + '/views/store-front.html'));
 });
+
 /**
  *
  * ============================AUTHENTICATION============================
  *
  */
-
-app.post('/auth', async (req, res, next) => {
+app.post('/auth', async (req, res) => {
   const authTokenData = await dotwallet.getUserToken(req.body.code, req.body.redirect_uri, true);
   const userAccessToken = authTokenData.access_token;
   console.log('userAccessToken', userAccessToken);
-  const userData = await dotwallet.getUserInfo(userAccessToken, true);
-  res.json({ ...userData, ...authTokenData });
+  const userInfo = await dotwallet.getUserInfo(userAccessToken, true);
+  res.json({ ...userInfo, ...authTokenData });
 });
 
 /**
@@ -52,13 +53,15 @@ app.post('/auth', async (req, res, next) => {
  * ============================PAYMENT============================
  *
  */
-
 app.post('/create-order', async (req, res) => {
   const order = { ...req.body };
   const orderID = await dotwallet.getOrderID(req.body, true);
+
+  // optional, check the order status:
   setTimeout(async () => {
     const orderStatus = await dotwallet.getOrderStatus(orderID, true);
     // console.log('orderStatus', orderStatus);
+    // optional, check the blockchain transaction
     const tx = await dotwallet.queryTx(orderStatus.txid, true);
     // console.log('tx', tx);
   }, 1000 * 60);
@@ -76,7 +79,6 @@ app.post('/payment-result', (req, res) => {
  * ============================AUTOMATIC PAYMENTS============================
  *
  */
-
 app.post('/autopay', async (req, res) => {
   const orderResultData = await dotwallet.autoPay(req.body, true);
   // console.log('orderResultData', orderResultData);
@@ -88,24 +90,16 @@ app.post('/autopay', async (req, res) => {
  * ============================SAVE DATA ON CHAIN============================
  *
  */
-
 const savedDataTxns = []; // In real app could store in DB. Save a list of txns to retrieve data
 
 app.post('/save-data', async (req, res) => {
-  try {
-    const data = req.body.saveData;
-    const userID = req.body.userID;
-    const saveData = await dotwallet.saveData(data, userID, undefined, true);
-    savedDataTxns.push({
-      ...saveData,
-      timestamp: new Date(),
-      tag: 'banana-price',
-    }); //in a real app this would go to DB
-    res.json(saveData);
-  } catch (error) {
-    console.log('==============error==============\n', error);
-    res.json(error);
-  }
+  const saveDataResult = await dotwallet.saveData(req.body.dataToSave, req.body.userID, undefined, true);
+  savedDataTxns.push({
+    ...saveDataResult,
+    timestamp: new Date(),
+    tag: 'banana-price',
+  }); //in a real app this would go to DB
+  res.json(saveDataResult);
 });
 
 app.post('/get-tx-data', async (req, res) => {
